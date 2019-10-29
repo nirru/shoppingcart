7db9113d6aaa927b565d2f6e446140a1d0a340f2
nirmal.k <nirmal.k@cisinlabs.com>
2019-10-29 13:45:13 +0530

    prod url added

--- src/server.js
+++ src/server.js
@@ -57,7 +57,6 @@ const DIST_DIR = path.join('dist');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-//Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
 
 //Send index.html when the user access the web
