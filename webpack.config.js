7db6a4aef39f0b0ef3aba55c46acbcc4e6d68c08
Nirmal <nirmal.kumar@daytonatec.com>
2020-03-03 04:23:28 +0530

    fix issue

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-        API_URL: JSON.stringify(process.env.API_URL),
       }
     }),
 
